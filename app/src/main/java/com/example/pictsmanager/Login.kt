30864c41ecc831434255b0b2feaec27b3e627011
MSI\Yazuke <simon-noel@orange.fr>
2021-06-17 18:03:45 +0200

    ajout du login
    admin en username et admin en mdp pour accéder à l'appli

---
+++ app/src/main/java/com/example/pictsmanager/Login.kt
@@ -0,0 +1,24 @@
+package com.example.pictsmanager
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login.*
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        button.setOnClickListener {
+            if(editTextUserName.text.toString() == "admin" && editTextTextPassword.text.toString() == "admin" ){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this,"Wrong username or password !", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
